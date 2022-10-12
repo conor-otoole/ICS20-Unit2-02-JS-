280b505abc2cd012e0cbe1bfc0ded3b8b6c09f59
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2022-10-12 18:22:25 +0000

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Conor All rights reserved
+//
+// Created by: Conor
+// Created on: Sep 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
